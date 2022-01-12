26336245608cad7474fa3075a3d8568bd657cb3c
kmh <dkakqlql@gmail.com>
2022-01-12 20:19:36 +0900

    Tutorial

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:tutorial/pages/navpages/main_page.dart';
+import 'package:tutorial/pages/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp()); // Start MyApp that is main page.
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage()
+    );
+  }
+}
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tutorial/misc/colors.dart';
+import 'package:tutorial/widgets/app_large_text.dart';
+import 'package:tutorial/widgets/app_text.dart';
+import 'package:tutorial/widgets/responsive_botton.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "images.jfif",
+    "cars.jfif",
+    "car.jfif"
+  ];
+  List text = [
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index){
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    images[index]
+                  ),
+                  fit: BoxFit.cover
+                )
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: "Example Title 1"),
+                        AppText(text: "Example Middle Word", size: 30),
+                        SizedBox(height: 20),
+                        Container(
+                          width: 250,
+                          child: AppText(
+                            text: "Example how to use this application",
+                            color: AppColors.textColor2,
+                            size: 14
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        ResponsiveButton()
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index==indexDots?25:8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index==indexDots?AppColors.mainColor:AppColors.mainColor.withOpacity(0.3)
+                          ),
+                        );
+                      })
+                    )
+                  ],
+                ),
+              ),
+            );
+      }),
+    );
+  }
+}
